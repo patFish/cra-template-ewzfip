228f8431967a301ccedb5184da1d7eb626e260f1
patFish <fischer.pa7rick@gmail.com>
2021-02-19 15:15:00 +0100

    FEAT: extracted Logo

---
+++ template/src/components/Logo.js
@@ -0,0 +1,10 @@
+import './style/Logo.css'
+import React from 'react'
+
+import logo from '../assets/icons/unicorn.png'
+
+const Logo = () => {
+  return <img src={logo} className="App-logo" alt="logo" />
+}
+
+export default Logo
